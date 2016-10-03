c72649f013bd1e69fc8546ccd5c5ba726eae51cb
Yashika Garg <yashikagarag13@gmail.com>
2016-10-03 13:43:03 +0530

    Lint file using standard

--- js/client-app.js
+++ js/client-app.js
@@ -1,3 +1,5 @@
+/* global React ReactDOM */
+
 var div = React.DOM.div
 var h1 = React.DOM.h1
 
@@ -9,16 +11,16 @@ var MyTitle = React.createClass({
       )
     )
   }
-});
+})
 
-var MyTitleFact = React.createFactory(MyTitle);
-var ce = React.createElement;
+var MyTitleFact = React.createFactory(MyTitle)
+var ce = React.createElement
 
 var MyFirstComponent = (
   div(null,
-    MyTitleFact({title: "Props are great", color: "red"}),
-    ce(MyTitle, {title: "Props are best", color: "blue"}),
-    React.createElement(MyTitle, {title: "My name is Yashika", color: "peru"})
+    MyTitleFact({title: 'Props are great', color: 'red'}),
+    ce(MyTitle, {title: 'Props are best', color: 'blue'}),
+    React.createElement(MyTitle, {title: 'My name is Yashika', color: 'peru'})
   )
-);
-ReactDOM.render(MyFirstComponent, document.getElementById("app"));
+)
+ReactDOM.render(MyFirstComponent, document.getElementById('app'))
