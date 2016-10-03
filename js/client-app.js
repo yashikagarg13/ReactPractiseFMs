81604dcba92600724e9ea4e9b3ffd0beff1c0e96
Yashika Garg <yashikagarag13@gmail.com>
2016-10-03 13:18:40 +0530

    Composite component and factories

--- js/client-app.js
+++ js/client-app.js
@@ -1,9 +1,24 @@
 var div = React.DOM.div
 var h1 = React.DOM.h1
 
+var MyTitle = React.createClass({
+  render () {
+    return (
+      div(null,
+        h1(null, "My Title component")
+      )
+    )
+  }
+});
+
+var MyTitleFact = React.createFactory(MyTitle);
+var ce = React.createElement;
+
 var MyFirstComponent = (
   div(null,
-    h1(null, "Hello world!!")
+    MyTitleFact(null),
+    ce(MyTitle, null),
+    React.createElement(MyTitle, null)
   )
 );
 ReactDOM.render(MyFirstComponent, document.getElementById("app"));
