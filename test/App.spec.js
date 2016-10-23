6349ff92ccecff90708f5c9c6fd86fb88f6c2576
Yashika Garg <yashika@powertofly.com>
2016-10-23 11:10:17 +0530

    Unit Testing - Render brand and show cards tests using shallow

--- test/App.spec.js
+++ test/App.spec.js
@@ -2,10 +2,20 @@
 /* eslint-env mocha */
 
 const {expect} = require('chai')
+const React = require('react')
+const Search = require('../js/Search')
+const ShowCard = require('../js/ShowCard')
+const {shows} = require('../public/data')
+const {shallow, mount} = require('enzyme')
 
 describe('<Search />', () => {
-	it("should pass", () => {
-		expect(((1 + 1) === 2)).to.be.true
-		expect(1 + 1).to.equal(2)
+	it("should render the brand", () => {
+		const wrapper = shallow(<Search />)
+		expect(wrapper.contains(<h1 className='brand'>svideo</h1>)).to.be.true
+	})
+
+	it('should render as many shows as there are data for', () => {
+		const wrapper = shallow(<Search />)
+		expect(wrapper.find(ShowCard).length).to.equal(shows.length)
 	})
 })
