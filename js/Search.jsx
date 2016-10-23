c9c643db92655728f34e8033296425e8f64cbae4
Yashika Garg <yashika@powertofly.com>
2016-10-23 09:19:35 +0530

    Managing State using getInitialState - Search Page - input search text field

--- js/Search.jsx
+++ js/Search.jsx
@@ -3,14 +3,36 @@ const Data = require('../public/data')
 
 const ShowCard = require('./ShowCard')
 
-const Search = () => (
-  <div className='container'>
-    <div className="shows">
-      {Data.shows.map(show => (
-        <ShowCard {...show} key={show.imdbID} />
-      ))}
-    </div>
-  </div>
-)
+// class Search extends React.Component {
+// The above method doesn't have autobinding
+const Search = React.createClass({
+  getInitialState () {
+    return {
+      searchTerm: 'Test search'
+    }
+  },
+  handleSearchTermEvent (event) {
+    this.setState({
+      searchTerm: event.target.value
+    })
+  },
+  render () {
+    return (
+      <div className='container'>
+        <header className='header'>
+          <h1 className='brand'>{this.state.searchTerm}</h1>
+          <input className='search-input' placeholder='Search' type='text'
+            value={this.state.searchTerm} onChange={this.handleSearchTermEvent} />
+        </header>
+        <div className="shows">
+          {Data.shows.map(show => (
+            <ShowCard {...show} key={show.imdbID} />
+          ))}
+        </div>
+      </div>
+    )
+  }
+})
+// }
 
 module.exports = Search
