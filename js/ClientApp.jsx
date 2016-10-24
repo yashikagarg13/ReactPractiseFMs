da0cd28ed8886a2c4fd4fac32a823e0a83f0d829
Yashika Garg <yashikagarag13@gmail.com>
2016-10-24 22:34:15 +0530

    Add Details route and pass id as route param

--- js/ClientApp.jsx
+++ js/ClientApp.jsx
@@ -5,12 +5,14 @@ const {Router, Route, IndexRoute, hashHistory} = require('react-router')
 const Layout = require('./Layout')
 const Landing = require('./Landing')
 const Search = require('./Search')
+const Details = require('./Details')
 
 const ClientApp = () => (
   <Router history={hashHistory}>
     <Route path="/" component={Layout}>
       <IndexRoute component={Landing} />
       <Route path='/search' component={Search} />
+      <Route path='/details/:id' component={Details} />
     </Route>
   </Router>
 )
