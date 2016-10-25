f828966df59c77e035b88b9a73c61f34f07995a7
Yashika Garg <yashikagarag13@gmail.com>
2016-10-26 00:42:40 +0530

    Design Details page

--- js/Details.jsx
+++ js/Details.jsx
@@ -3,18 +3,30 @@ const {object} = React.PropTypes
 
 class Details extends React.Component {
   render () {
+    const {title, description, year, poster, trailer} = this.props.params || {}
+
     return (
-      <div style={{textAlign: 'left'}} className='container'>
-        <pre><code>
-          {JSON.stringify(this.props.params, null, 4)}
-        </code></pre>
+      <div className='container'>
+        <header className='header'>
+          <h1 className='brand'>svideo</h1>
+        </header>
+        <div className='video-info'>
+          <h1 className='video-title'>{title}</h1>
+          <h2 className='video-year'>({year})</h2>
+          <img className='video-poster' src={`public/img/posters/${poster}`} />
+          <p className='video-description'>{description}</p>
+        </div>
+        <div className='video-container'>
+          <iframe src={`https://www.youtube-nocookie.com/embed/${trailer}?rel=0&amp;controls=0&amp;showinfo=0`}
+            frameBorder='0' allowFullScreen></iframe>
+        </div>
       </div>
     )
   }
 }
 
 Details.propTypes = {
-  params: object
+  params: object.isRequired
 }
 
 module.exports = Details
