e06de92163631ed3b90818f58b658c4b417db769
Yashika Garg <yashika@powertofly.com>
2016-11-03 14:23:29 +0530

    Make LAnding page work - take search input and redirect to search

--- js/Landing.jsx
+++ js/Landing.jsx
@@ -1,12 +1,43 @@
 const React = require('react')
+const {hashHistory} = require('react-router')
+const {func, string} = React.PropTypes
 const {Link} = require('react-router')
 
-const Landing = () => (
-  <div className="home-info">
-    <h1 className="title">SVideo</h1>
-    <input type="text" placeholder="Search" className="search" />
-    <Link to="/search" className="browse-all">or Browse All</Link>
-  </div>
-)
+const {connector} = require('./Store')
 
-module.exports = Landing
+class Landing extends React.Component {
+  constructor (props) {
+    super(props)
+    this.handleSearchTermEvent = this.handleSearchTermEvent.bind(this)
+    this.goToSearch = this.goToSearch.bind(this)
+  }
+
+  handleSearchTermEvent (event) {
+    this.props.setSearchTerm(event.target.value)
+  }
+  goToSearch (event) {
+    event.preventDefault()
+
+    hashHistory.push('search')
+  }
+
+  render () {
+    return (
+      <div className="home-info">
+        <h1 className="title">SVideo</h1>
+        <form onSubmit={this.goToSearch}>
+          <input type="text" placeholder="Search" className="search"
+            value={this.props.searchTerm} onChange={this.handleSearchTermEvent} />
+        </form>
+        <Link to="/search" className="browse-all">or Browse All</Link>
+      </div>
+    )
+  }
+}
+
+Landing.propTypes = {
+  searchTerm: string,
+  setSearchTerm: func
+}
+
+module.exports = connector(Landing)
