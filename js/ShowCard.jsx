ba2c903b693c5e9846ae0c5973eedb6af6e403cf
Yashika Garg <yashika@powertofly.com>
2016-11-03 14:27:58 +0530

    Connect Show cards to card details component

--- js/ShowCard.jsx
+++ js/ShowCard.jsx
@@ -1,22 +1,26 @@
 const React = require('react')
+const {Link} = require('react-router')
 const {string} = React.PropTypes
 
-const ShowCard = (props) => {
-  return <div className='show-card'>
-    <img src={`public/img/posters/${props.poster}`} className='show-card-img' />
-    <div className='show-card-text'>
-      <h3 className='show-card-title'>{props.title}</h3>
-      <h4 className='show-card-year'>({props.year})</h4>
-      <p className='show-card-description'>{props.description}</p>
+const ShowCard = (props) => (
+  <Link to={`/details/${props.imdbID}`}>
+    <div className='show-card'>
+      <img src={`public/img/posters/${props.poster}`} className='show-card-img' />
+      <div className='show-card-text'>
+        <h3 className='show-card-title'>{props.title}</h3>
+        <h4 className='show-card-year'>({props.year})</h4>
+        <p className='show-card-description'>{props.description}</p>
+      </div>
     </div>
-  </div>
-}
+  </Link>
+)
 
 ShowCard.propTypes = {
   poster: string.isRequired,
   title: string.isRequired,
   year: string.isRequired,
-  description: string.isRequired
+  description: string.isRequired,
+  imdbID: string.isRequired
 }
 
 module.exports = ShowCard
